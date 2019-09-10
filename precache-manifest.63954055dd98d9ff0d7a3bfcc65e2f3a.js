3aa368aa6350d10b07187b75ce2811801281de9a
unknown <rajankarmakar@hotmail.comm>
2019-09-10 19:21:24 +0600

    Updates

---
+++ precache-manifest.63954055dd98d9ff0d7a3bfcc65e2f3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649c0a00e3e877dbea7c1860302d0f4e",
+    "url": "/comingsoon/index.html"
+  },
+  {
+    "revision": "f5b0d79d958043bcaab8",
+    "url": "/comingsoon/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9f3d5acebd46ec3a93b6",
+    "url": "/comingsoon/static/css/main.83af0d5a.chunk.css"
+  },
+  {
+    "revision": "f5b0d79d958043bcaab8",
+    "url": "/comingsoon/static/js/2.207a5d25.chunk.js"
+  },
+  {
+    "revision": "9f3d5acebd46ec3a93b6",
+    "url": "/comingsoon/static/js/main.62a5b323.chunk.js"
+  },
+  {
+    "revision": "c35757932bee3d8a6b16",
+    "url": "/comingsoon/static/js/runtime~main.736b6abe.js"
+  }
+]);
